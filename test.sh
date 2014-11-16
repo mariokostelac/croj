72f6471e12b2d7ea710655200e280076645d68a7
Mario Kostelac <mario.kostelac@gmail.com>
2014-11-16 18:41:58 +0000

    fixed forwarding output

--- test.sh
+++ test.sh
@@ -11,7 +11,7 @@ if [[ $timelimit == "" ]]; then
 fi
 for in in $(find /test_data -name "$file_pattern"); do
     out=${in/.in./.out.}
-    (time -p (./croj/timeout.sh "$timelimit" -- ./croj/tmp/a.out < $in > /croj/tmp/last_output))  > /croj/tmp/time 2>&1
+    (time -p (./croj/timeout.sh "$timelimit" bash -c "./croj/tmp/a.out < $in > /croj/tmp/last_output" )) > /croj/tmp/time 2>&1
     tle=$?
     exec_time=$(cat /croj/tmp/time | grep real | cut -d' ' -f 2)
     diffs=$(diff -w $out /croj/tmp/last_output)
